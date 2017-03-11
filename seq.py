12a733591f753e495e004674a463ee5280259a2c
Eric Wustrow <ewust@colorado.edu>
2017-03-10 18:50:56 -0700

    Allow bi-directional send

--- seq.py
+++ seq.py
@@ -5,6 +5,7 @@ import time
 import struct
 import sys
 from optparse import OptionParser
+from threading import Thread
 
 
 
@@ -50,52 +51,75 @@ extra = ''
 
 last_time = time.time()
 
-def occasional_print(n):
+def occasional_print(n, send=False):
     global last_time
-    if (n % (256*1024)) == 0:
+    if (n % (16*1024)) == 0:
         now = time.time()
-        bw = (256*1024*4)/(now - last_time)
-        print '%.06f ---> %08x   %.3f MB/s' % (now, n, bw/1000000)
+        bw = (16*1024)/(now - last_time)
+        print '%s%.06f %s %08x   %.3f MB/s' % ('  ' if send else '', now, '-sent->' if send else '<-recv-', n, bw/1000000)
         last_time = now
 
 
 BUF_SIZE = 1024
+SEND_SIZE = 1024*1024*20    # 20MB
 
-if send:
+
+def send_thread(conn, delay):
     ##### SEND
+    global SEND_SIZE, BUF_SIZE
     n = 0
-    for i in xrange(1024*20*1024/(BUF_SIZE/4)):
+    for i in xrange(SEND_SIZE/(BUF_SIZE/4)):
         buf = ''
         for j in xrange(BUF_SIZE/4):
             n += 1
             buf += struct.pack('!I', n)
 
-            occasional_print(n)
+            occasional_print(n, True)
 
         conn.sendall(buf)
+        time.sleep(delay)
 
-else:
-    ##### RECEIVE
+    conn.shutdown(socket.SHUT_WR)
+
+
+thread = None
+if send:
+    thread = Thread(target=send_thread, args = (conn, 0))
+    thread.start()
+    #thread.join()
+
+
+##### RECEIVE
+extra = ''
+last_buf = ''
+last_n = 0
+while True:
+    buf = conn.recv(BUF_SIZE)
+    buf = extra + buf
+    if (buf == ''):
+        print 'Closed at %08x' % last_n
+        break
+    for i in xrange(len(buf)/4):
+        n, = struct.unpack('!I', buf[4*i:4*i+4])
+        #if n == 0x1000:
+            #print 'Calling shutdown...'
+            #conn.shutdown(1)
+            #time.sleep(5)
+        if n != (last_n + 1) and n != 0:
+            print '=========ERROR: expected %08x got %08x at offset %d (len %d)' % (last_n+1, n, 4*i, len(buf))
+            print ''
+            print '----last buf:'
+            print last_buf.encode('hex')
+            print '----this buf:'
+            print buf.encode('hex')
+            sys.exit(1)
+        last_n = n
+
+        occasional_print(n)
+    last_buf = buf
     extra = ''
-    last_buf = ''
-    last_n = 0
-    while True:
-        buf = conn.recv(BUF_SIZE)
-        buf = extra + buf
-        for i in xrange(len(buf)/4):
-            n, = struct.unpack('!I', buf[4*i:4*i+4])
-            if n != (last_n + 1) and n != 0:
-                print '=========ERROR: expected %08x got %08x at offset %d (len %d)' % (last_n+1, n, 4*i, len(buf))
-                print ''
-                print '----last buf:'
-                print last_buf.encode('hex')
-                print '----this buf:'
-                print buf.encode('hex')
-                sys.exit(1)
-            last_n = n
+    if (len(buf)%4) != 0:
+        extra = buf[-(len(buf) % 4):]
 
-            occasional_print(n)
-        last_buf = buf
-        extra = ''
-        if (len(buf)%4) != 0:
-            extra = buf[-(len(buf) % 4):]
+if thread is not None:
+    thread.join()
